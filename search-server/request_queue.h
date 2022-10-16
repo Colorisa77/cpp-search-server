d6df09ac3c927ff7786b1c77d6e1f6bac28bb81f
Colorisa77 <nikcola777@gmail.com>
2022-10-17 02:16:00 +1000

    fifth sprint #1

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -6,17 +6,14 @@
 
 class RequestQueue {
 public:
-    explicit RequestQueue(const SearchServer& search_server)
+    RequestQueue(const SearchServer& search_server)
         : search_server_(search_server)
         , no_result_requests_(0)
         , current_time_(0) {
     }
+
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
-        AddRequest(result.size());
-        return result;
-    }
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
     std::vector<Document> AddFindRequest(const std::string& raw_query);
     int GetNoResultRequests() const;
@@ -33,3 +30,10 @@ private:
 
     void AddRequest(int results_num);
 };
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
+    RequestQueue::AddRequest(result.size());
+    return result;
+}
